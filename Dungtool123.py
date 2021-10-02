534cb8877398aa3fc4beec762a880214a05d11f8
Dung3563 <91686770+Dung3563@users.noreply.github.com>
2021-10-02 12:56:21 +0700

    Add files via upload

--- Dungtool123.py
+++ Dungtool123.py
@@ -25,7 +25,7 @@ print("bạn sai, đưa tôi 2$. bạn thắng, tôi đưa 20$")
 #GuessingGame.py
 import random # phải “nhập khẩu” chương trình random trước
 the_number = random.randint(1, 10)
-guess = int(input("Guess a number between 1 and 10: ")) # bạn đoán mò 1 số từ 1 đến 10
+guess = input("Guess a number between 1 and 10: ") # bạn đoán mò 1 số từ 1 đến 10
 count = 0
 while guess != the_number: # trong khi số bạn cho còn khác với số của máy cho thì cứ tiếp tục vòng lặp while
     if guess > the_number:
@@ -34,16 +34,16 @@ while guess != the_number: # trong khi số bạn cho còn khác với số củ
     if guess < the_number:
         print(guess, "sai rồi đưa t 2$ :).") # không phải, số m chọn thấp quá,đưa t 2$
         count = count + 2
-    guess = int(input("lại đê bạn êi :)")) # Đoán lại đi !
+    guess = input("lại đê bạn êi :)") # Đoán lại đi !
 print(guess, "đúng rồi :( 20$ nè") # Đúng rồi m thắng 20$ :(((
 if count < 20:
   print("But we pay", 20-count, "$")
 else:
   print("But you still have to pay", 20-count, "$")
   
-  print("tiếp theo là trò chơi con mực lỗi p2 :))")
+print("tiếp theo là trò chơi con mực lỗi p2 :))")
   
-  print("trò chơi này tên là oẳn tù tì (búa bao kéo)")
+print("trò chơi này tên là oẳn tù tì (búa bao kéo)")
   
 
 import random   # “nhập cảng” đúng ra là gọi chương trình random của Python 
