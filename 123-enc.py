6b687cf634df907a23d1f94c8f96e12c79f181cd
Dung3563 <91686770+Dung3563@users.noreply.github.com>
2021-11-10 19:15:51 +0700

    Add files via upload

--- 123-enc.py
+++ 123-enc.py
@@ -43,7 +43,7 @@ def login():
                 print ('Wrong Key')
         else:
             print ('Wrong Username Key')
-def menu():
+def main_menu():
   os.system('clear')
   print(banner) 
   print(f) 
